515fb41a0afad06d60097c1e46b85b61a4a0f2c3
Sazonov Georgy <gera950@inbox.ru>
2024-11-04 20:52:05 +0300

    Fix generate

--- SLAU_solver.cpp
+++ SLAU_solver.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream> // TEST
 #include <fstream>
 
 using namespace std;
@@ -14,12 +15,17 @@ int oldInd2New(int Nx, int Ny, int K1, int K2, int i, int j){
     return new_I;
 }
 
-void generate(int Nx, int Ny, int K1, int K2, int& N, int** IA, int** JA){
-    N = oldInd2New(Nx, Ny, K1, K2, Ny - 1, Nx - 1);
-    int doubled_E = N; // сразу плюсуем главную диагональ
+void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
     int K = K1 + K2;
+    N = oldInd2New(Nx, Ny, K1, K2, Ny - 1, Nx - 1) + 1;
+    if ((Ny - 1) * Nx + (Nx - 1) % K >= K1){
+        ++N;
+    }
+    int doubled_E = N; // сразу плюсуем главную диагональ
+    cout << "doubled_E: " << doubled_E << endl;
     int cnt_IA = 0, cnt_JA = 0;
     int new_I;
+    bool split;
 
     for(int i = 0; i < Ny; ++i){
         for(int j = 0; j < Nx; ++j){
@@ -41,50 +47,128 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int** IA, int** JA){
         }
     }
 
-    *IA = new int[N + 2]; // + 2 т.к. ещё резервируем под последний элемент, равный размеру JA
-    *JA = new int[doubled_E];
+    IA = new int[N + 1]; // + 2 т.к. ещё резервируем под последний элемент, равный размеру JA
+    JA = new int[doubled_E];
+
+    // cout << N + 2 << " " << doubled_E << endl;
 
     for(int i = 0; i < Ny; ++i){
         for(int j = 0; j < Nx; ++j){
-            new_I = oldInd2New(Nx, Ny, K1, K2, i, j);
-            *IA[cnt_IA] = cnt_JA;
-            if (i - 1 >= 0) {
-                if (((i - 1) * Nx + j) % K >= K1){ // если верхний сосед разбивается на треугольник - соеднинение с правым треугольником
-                    *JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j) + 1;
+            split = (i * Nx + j) % K >= K1;
+            if (split){ // если клетка разбивается на треугольники - обработать их вместе
+                new_I = oldInd2New(Nx, Ny, K1, K2, i, j);
+                IA[cnt_IA] = cnt_JA;
+                if (i - 1 >= 0) {
+                    if (((i - 1) * Nx + j) % K >= K1){ // если верхний сосед разбивается на треугольник - соеднинение с правым треугольником
+                        JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j) + 1;
+                    }
+                    else{
+                        JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j);
+                    }
+                    ++cnt_JA;
                 }
-                else{
-                    *JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j);
+                if (j - 1 >= 0) { // левый сосед
+                    JA[cnt_JA] = new_I - 1;
+                    ++cnt_JA;
                 }
+                // добавление связи "с собой" - главная диагональ
+                JA[cnt_JA] = new_I;
                 ++cnt_JA;
-            }
-
-            if (j - 1 >= 0) { // левый сосед
-                *JA[cnt_JA] = new_I - 1;
+                JA[cnt_JA] = new_I + 1; // правый сосед точно есть
                 ++cnt_JA;
-            }
+                ++cnt_IA;
+                ++new_I;
 
-            // добавление связи "с собой" - главная диагональ
-            *JA[cnt_JA] = new_I;
-            ++cnt_JA;
-
-            if (j + 1 < Nx) { // правый сосед
-                *JA[cnt_JA] = new_I + 1;
+                IA[cnt_IA] = cnt_JA;
+                JA[cnt_JA] = new_I - 1; // левый сосед точно есть
                 ++cnt_JA;
-            }
-
-            if (i + 1 < Ny) { // нижний сосед
-                *JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i + 1, j);
+                // добавление связи "с собой" - главная диагональ
+                JA[cnt_JA] = new_I;
                 ++cnt_JA;
+                if (j + 1 < Nx) { // правый сосед
+                    JA[cnt_JA] = new_I + 1;
+                    ++cnt_JA;
+                }
+                if (i + 1 < Ny) { // нижний сосед
+                    JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i + 1, j);
+                    ++cnt_JA;
+                }
+                ++cnt_IA;
             }
-            ++cnt_IA;
+            else{
+                new_I = oldInd2New(Nx, Ny, K1, K2, i, j);
+                IA[cnt_IA] = cnt_JA;
+                if (i - 1 >= 0) {
+                    if (((i - 1) * Nx + j) % K >= K1){ // если верхний сосед разбивается на треугольник - соеднинение с правым треугольником
+                        JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j) + 1;
+                    }
+                    else{
+                        JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i - 1, j);
+                    }
+                    ++cnt_JA;
+                }
+
+                if (j - 1 >= 0) { // левый сосед
+                    JA[cnt_JA] = new_I - 1;
+                    ++cnt_JA;
+                }
+
+                // добавление связи "с собой" - главная диагональ
+                JA[cnt_JA] = new_I;
+                ++cnt_JA;
+
+                if (j + 1 < Nx) { // правый сосед
+                    JA[cnt_JA] = new_I + 1;
+                    ++cnt_JA;
+                }
+
+                if (i + 1 < Ny) { // нижний сосед
+                    JA[cnt_JA] = oldInd2New(Nx, Ny, K1, K2, i + 1, j);
+                    ++cnt_JA;
+                }
+                ++cnt_IA;
+            }
         }
     }
-    *IA[cnt_IA] = doubled_E; // последний элемент IA - число ненулевых в портрете
+    cout <<"doubled_E: " << doubled_E << "  cnt_JA: " <<  cnt_JA << endl;
+
+    // cout << cnt_IA << "   KEK    "<< cnt_JA << endl;
+    IA[cnt_IA] = doubled_E; // последний элемент IA - число ненулевых в портрете
+    // cout << "TEST 4" << endl;
 }
 
 
 
 int main(int argc, char** argv){
-    
+    int Nx, Ny, K1, K2, N, doubled_E;
+    int *IA, *JA;
+    ofstream logFile("error_log.txt", ios::out);
+    if (!logFile.is_open()){
+        return 1;
+    }
+    if (argc < 2){
+        // TODO print help
+    }
+    ifstream inFile(argv[1], ios::in);
+    inFile >> Nx >> Ny >> K1 >> K2;
+
+    generate(Nx, Ny, K1, K2, N, IA, JA);
+    for (int i = 0; i < N+1; ++i){
+        logFile << IA[i] << " ";
+    }
+    logFile << endl;
+    doubled_E = IA[N];
+    cout << doubled_E << endl;
+    for (int i = 0; i < doubled_E; ++i){
+        logFile << JA[i] << " ";
+    }  
+
+    logFile.close();
+    inFile.close();
+
+    delete [] IA;
+    cout << "TEST Y" << endl;
+    delete [] JA;
+    cout << "TEST X" << endl;
     return 0;
 }
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+prog="lab1"
+ans_file="out.txt"
+tmp_file=".tmp_ans"
+
+truncate -s 0 $tmp_file
+while IFS=' ' read -r c1 c2 c3 c4 ; do
+  `./$prog $c1 $c2 $c3 $c4 >> $tmp_file`
+  echo "" >> $tmp_file 
+done <in.txt
+
+cmp -b $tmp_file $ans_file
