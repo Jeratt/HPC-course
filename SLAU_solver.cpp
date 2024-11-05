91ccffd9a4af9b7c97007c096b6b807d7e603c81
Sazonov Georgy <gera950@inbox.ru>
2024-11-05 12:54:36 +0300

    Add OMP

--- SLAU_solver.cpp
+++ SLAU_solver.cpp
@@ -2,6 +2,7 @@
 #include <iostream> // TEST
 #include <fstream>
 #include <cmath>
+#include <omp.h>
 
 using namespace std;
 
@@ -25,7 +26,9 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
     //int* v_types = new int[N];
     int* cnt_neigh = new int[N];
 
+    #pragma omp parallel
     for(int i = 0; i < Ny; ++i){
+        int doubled_E_local = 0;
         for(int j = 0; j < Nx; ++j){
             int new_I = oldInd2New(Nx, Ny, K1, K2, i, j);
             if ((i * Nx + j) % K >= K1){
@@ -39,7 +42,7 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
                 if (j - 1 >= 0){ // левый сосед
                     ++cnt_neigh[new_I];
                 }
-                doubled_E += cnt_neigh[new_I];
+                doubled_E_local += cnt_neigh[new_I];
 
                 cnt_neigh[new_I + 1] = 2;
                 if (i + 1 < Ny){ // нижний сосед
@@ -48,7 +51,7 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
                 if (j + 1 < Nx){
                     ++cnt_neigh[new_I + 1]; // правый сосед
                 }
-                doubled_E += cnt_neigh[new_I + 1];
+                doubled_E_local += cnt_neigh[new_I + 1];
             }
             else{
                 //v_types[new_I] = 0; // обычная клетка
@@ -65,7 +68,16 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
                 if (j + 1 < Nx){
                     ++cnt_neigh[new_I]; // правый сосед
                 }
-                doubled_E += cnt_neigh[new_I];
+                doubled_E_local += cnt_neigh[new_I];
+            }
+        }
+
+        const int nt = omp_get_num_threads();
+        const int tn = omp_get_thread_num();
+        for(int i = 0; i < nt; ++i){
+            #pragma omp barrier
+            if (i == tn){
+                doubled_E += doubled_E_local;
             }
         }
     }
@@ -81,6 +93,7 @@ void generate(int Nx, int Ny, int K1, int K2, int& N, int*& IA, int*& JA){
     }
     IA[N] = doubled_E;
 
+    #pragma omp parallel
     for(int i = 0; i < Ny; ++i){
         for(int j = 0; j < Nx; ++j){
             int new_I = oldInd2New(Nx, Ny, K1, K2, i, j);
@@ -154,6 +167,7 @@ void fill(int N, int*& IA, int*& JA, double*& A, double*& b){
     A = new double[IA[N]];
     b = new double[N];
 
+    #pragma omp parallel
     for (int i = 0; i < N; ++i){
         for (int j = IA[i]; j < IA[i + 1]; ++j){
             if (i == JA[j]){
@@ -165,6 +179,7 @@ void fill(int N, int*& IA, int*& JA, double*& A, double*& b){
         }
     }
 
+    #pragma omp parallel
     for (int i = 0; i < N; ++i){
         for (int j = IA[i]; j < IA[i + 1]; ++j){
             if (i == JA[j]){
@@ -174,6 +189,7 @@ void fill(int N, int*& IA, int*& JA, double*& A, double*& b){
         }
     }
 
+    #pragma omp parallel
     for (int i = 0; i < N; ++i){
         A[diag[i]] *= DIAG_COEFF;
         b[i] = sin(i);
@@ -183,7 +199,7 @@ void fill(int N, int*& IA, int*& JA, double*& A, double*& b){
 
 
 int main(int argc, char** argv){
-    int Nx, Ny, K1, K2, N, doubled_E;
+    int Nx, Ny, K1, K2, N, doubled_E, T;
     int *IA, *JA;
     double *A, *b;
     ofstream logFile("error_log.txt", ios::out);
@@ -194,7 +210,9 @@ int main(int argc, char** argv){
         // TODO print help
     }
     ifstream inFile(argv[1], ios::in);
-    inFile >> Nx >> Ny >> K1 >> K2;
+    inFile >> Nx >> Ny >> K1 >> K2 >> T;
+
+    omp_set_num_threads(T);
 
     generate(Nx, Ny, K1, K2, N, IA, JA);
     for (int i = 0; i < N+1; ++i){
