d98d656bceeecf8daa472b4454241a100398ca18
Sazonov Georgy <gera950@inbox.ru>
2024-11-20 21:31:07 +0300

    fix script loader

--- script_maker.py
+++ script_maker.py
@@ -7,9 +7,14 @@ for n in N_grid:
             fin.write("#BSUB -J \"OpenMP_job\"\n")
             fin.write("#BSUB -o \"OpenMP_job%J.out\"\n")
             fin.write("#BSUB -e \"OpenMP_job%J.err\"\n")
-            if t < 16:
+            if t < 20:
                 fin.write(f"#BSUB -R \"affinity[core({t})]\"\n")
             else:
-                fin.write(f"#BSUB -R \"affinity[core(8)]\"\n")
-                fin.write(f"OMP_NUM_THREADS={t}\n")
+                fin.write(f"#BSUB -R \"affinity[core({16})]\"\n")
+            fin.write(f"OMP_NUM_THREADS={t}\n")
+            # if t < 16:
+            #     fin.write(f"#BSUB -R \"affinity[core({t})]\"\n")
+            # else:
+            #     fin.write(f"#BSUB -R \"affinity[core(8)]\"\n")
+            #     fin.write(f"OMP_NUM_THREADS={t}\n")
             fin.write(f"/polusfs/lsf/openmp/launchOpenMP.py ../a.out {n} {n} 3 3 {t}")
