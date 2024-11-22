89bfa12f4411fed74e30254befcd2619361398fc
Sazonov Georgy <gera950@inbox.ru>
2024-11-22 17:12:52 +0300

    Return test output for solve

--- SLAU_solver.cpp
+++ SLAU_solver.cpp
@@ -89,22 +89,22 @@ double solve(int N, int*& IA, int*& JA, double*& A, double*& b, double eps, int 
     vector_cp(N, r_k_prev, b); // r_0
 
     // TEST
-    // cout << "doubled_E: " << IA[N] << endl;
+    cout << "doubled_E: " << IA[N] << endl;
 
-    // t = omp_get_wtime();
-    // SpMv(N, IA, JA, M, r_k_prev, z_k);
-    // t = omp_get_wtime() - t;
-    // cout << "SpMv took: " << setprecision(5) << t << " seconds" << endl;
+    t = omp_get_wtime();
+    SpMv(N, IA, JA, M, r_k_prev, z_k);
+    t = omp_get_wtime() - t;
+    cout << "SpMv took: " << setprecision(5) << t << " seconds" << endl;
 
-    // t = omp_get_wtime();
-    // axpy(N, 1.23, r_k_prev, x_k_prev, p_k);
-    // t = omp_get_wtime() - t;
-    // cout << "axpy took: " << setprecision(5) << t << " seconds" << endl;
+    t = omp_get_wtime();
+    axpy(N, 1.23, r_k_prev, x_k_prev, p_k);
+    t = omp_get_wtime() - t;
+    cout << "axpy took: " << setprecision(5) << t << " seconds" << endl;
 
-    // t = omp_get_wtime();
-    // dot(N, r_k_prev, x_k_prev);
-    // t = omp_get_wtime() - t;
-    // cout << "dot took: " << setprecision(5) << t << " seconds" << endl;
+    t = omp_get_wtime();
+    dot(N, r_k_prev, x_k_prev);
+    t = omp_get_wtime() - t;
+    cout << "dot took: " << setprecision(5) << t << " seconds" << endl;
 
 
     do{
