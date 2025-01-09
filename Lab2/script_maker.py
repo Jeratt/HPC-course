64bef197243cc465cfb8fe85c7370ff067731e6d
Sazonov Georgy <gera950@inbox.ru>
2025-01-09 14:45:07 +0300

    +

--- Lab2/script_maker.py
+++ Lab2/script_maker.py
@@ -5,7 +5,10 @@ p_grid = [1, 2, 4, 8, 16, 32]
 for n in N_grid:
     for p in p_grid:
         with open(f"solver_job_{n}_{p}.lsf", 'w') as fin:
-            fin.write(f"#BSUB -n {p}\n")
+            if p > 1:
+                fin.write(f"#BSUB -n {p//2} -R \"span[ptile=2]\"\n")
+            else:
+                fin.write(f"#BSUB -n {p}\n")
             fin.write("#BSUB -J \"OpenMP_job\"\n")
             fin.write("#BSUB -o \"OpenMP_job%J.out\"\n")
             fin.write("#BSUB -e \"OpenMP_job%J.err\"\n")
